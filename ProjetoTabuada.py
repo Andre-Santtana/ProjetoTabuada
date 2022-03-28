5da939e0c80160da504ad001ba2632fef201620e
Andre-Santtana <58348493+Andre-Santtana@users.noreply.github.com>
2022-03-27 21:25:26 -0300

    Inicio do projeto

---
+++ ProjetoTabuada.py
@@ -0,0 +1,23 @@
+def tabuadaSoma():
+    numMax = int(input("Inforne a quantidade de linhas da tabuada: "))
+    numTab = int(input("Informe qual o nunero que quer gerar a tabuada: "))
+
+    for i in range(numMax):
+        print(f"{i} + {numTab} = {i + numTab}")
+
+
+print("=============")
+print("=  TABUADA  =")
+print("=============")
+
+print("\nEsse programa gera as seguintes tabuadas: Adição, Subtração, Multiplicação e Divisão \n\n")
+
+print("1-Adição \n2-Subtracao \n3-Multiplicacao \n4-Divisao ")
+
+opc_escolhida = int(input("\nEscolha a opção desejada da tabuada: "))
+
+if opc_escolhida:
+    if opc_escolhida == 1:
+        tabuadaSoma()
+    if opc_escolhida == 0:
+        print("Exite")
